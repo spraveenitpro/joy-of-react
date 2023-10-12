78fa31107176a56dad93d569792bdfeccfba46cf
Praveen <spraveenitpro@gmail.com>
2023-10-11 22:34:18 -0700

    Daily update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./styles.css";
 
 import Whitespaces from "./Whitespaces";
 import Expressionslot from "./Expressionslot";
+import FriendlyGreeting from "./FriendlyGreeting";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Joy of React exercises</h1>
-      <Whitespaces />
-      <Expressionslot />
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Joy of React exercises</h1>
+			<Whitespaces />
+			<Expressionslot />
+			<FriendlyGreeting />
+		</div>
+	);
 }
---
+++ src/FriendlyGreeting.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function FriendlyGreeting() {
+	return (
+		<p style={
+			{
+				fontSize: '1.25rem',
+				textAlign: 'center',
+				color: 'sienna',
+			}
+		}>
+			Greetings, weary traveller!
+		</p>
+	)
+}
+
+export default FriendlyGreeting
