be574526cb2cdec04f1f82d665b1e7f04287b55b
Praveen <spraveenitpro@gmail.com>
2023-10-13 08:07:43 -0700

    Daily update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import Whitespaces from "./Whitespaces";
 import Expressionslot from "./Expressionslot";
 import FriendlyGreeting from "./FriendlyGreeting";
 
+function RedButton({ children }) {
+	return (
+		<button
+			style={{
+				color: 'white',
+				backgroundColor: 'red'
+			}}
+		>
+			{children}
+		</button>
+	);
+}
+
 export default function App() {
 	return (
 		<div className="App">
 			<h1>Joy of React exercises</h1>
 			<Whitespaces />
 			<Expressionslot />
-			<FriendlyGreeting />
+			<FriendlyGreeting name="Praveen" />
+
+			<RedButton>Don't click me</RedButton>
 		</div>
 	);
 }
--- src/FriendlyGreeting.jsx
+++ src/FriendlyGreeting.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 
-function FriendlyGreeting() {
-	return (
-		<p style={
-			{
-				fontSize: '1.25rem',
-				textAlign: 'center',
-				color: 'sienna',
-			}
-		}>
-			Greetings, weary traveller!
-		</p>
-	)
+/**
+ * Converts a time string in the format "HH:MM" to the total number of minutes.
+ * @param {string} timeString - A string representing a time in the format "HH:MM".
+ * @returns {number} - The total number of minutes.
+ */
+
+function FriendlyGreeting({ name = 'There!' }) {
+    return (
+        <p style={
+            {
+                fontSize: '1.25rem',
+                textAlign: 'center',
+                color: 'sienna',
+            }
+        }>
+            Greetings, {name}!
+        </p>
+    )
 }
 
 export default FriendlyGreeting
