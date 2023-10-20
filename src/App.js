e9914a9e7d686c67d73f7519bda6afb21a3a1bb2
Praveen <spraveenitpro@gmail.com>
2023-10-19 23:19:54 -0700

    Daily update

--- src/App.js
+++ src/App.js
@@ -8,50 +8,60 @@ import ContactCard from "./ContactCard";
 import MyButton from "./MyButton";
 
 const data = [
-	{
-		id: 'sunita-abc123',
-		name: 'Sunita Kumar',
-		job: 'Electrical Engineer',
-		email: 'sunita.kumar@acme.co',
-	},
-	{
-		id: 'henderson-def456',
-		name: 'Henderson G. Sterling II',
-		job: 'Receptionist',
-		email: 'henderson-the-second@acme.co',
-	},
-	{
-		id: 'aio-ghi789',
-		name: 'Aoi Kobayashi',
-		job: 'President',
-		email: 'kobayashi.aoi@acme.co',
-	},
+    {
+        id: 'sunita-abc123',
+        name: 'Sunita Kumar',
+        job: 'Electrical Engineer',
+        email: 'sunita.kumar@acme.co',
+    },
+    {
+        id: 'henderson-def456',
+        name: 'Henderson G. Sterling II',
+        job: 'Receptionist',
+        email: 'henderson-the-second@acme.co',
+    },
+    {
+        id: 'aio-ghi789',
+        name: 'Aoi Kobayashi',
+        job: 'President',
+        email: 'kobayashi.aoi@acme.co',
+    },
 ]
 
 export default function App() {
-	return (
-		<div className="App">
-			<h1>Joy of React exercises</h1>
-			<Whitespaces />
-			<Expressionslot />
-			<FriendlyGreeting name="Praveen" />
+    return (
+        <div className="App">
+            <h1>Joy of React exercises</h1>
+            <Whitespaces />
+            <Expressionslot />
+            <FriendlyGreeting name="Praveen" />
 
-			<RedButton content="Dont click" />
+            <RedButton content="Dont click" />
 
-			<MyButton status="cancel" > Cancel </MyButton>
-			<MyButton status="confirm"> Confirm </MyButton>
-
-			<ul>
-				{
-					data.map((contact) => (
-						<ContactCard name={contact.name} job={contact.job} email={contact.email} />
-					))
-				}
-			</ul>
+            <MyButton status="cancel" > Cancel </MyButton>
+            <MyButton status="confirm"> Confirm </MyButton>
 
 
 
+            <ul>
+                {
+                    data.map((contact) => {
+                        return <ContactCard name={contact.name} job={contact.job} email={contact.email} />
+                    })
+                }
+            </ul>
 
-		</div>
-	);
+            <ul>
+                {
+                    data.map((contact) => (
+                        <ContactCard name={contact.name} job={contact.job} email={contact.email} />
+                    ))
+                }
+            </ul>
+
+
+
+
+        </div>
+    );
 }
