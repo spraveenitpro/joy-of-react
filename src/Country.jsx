91417a6cb3837ab3bb1fa1bf1d03588f81ecfad0
Praveen <spraveenitpro@gmail.com>
2023-11-09 07:54:22 -0800

    Daily update

--- src/Country.jsx
+++ src/Country.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 import { COUNTRIES } from './data'
 
 function Country() {
+	const CORRECT_CODE = '123456';
 	const [country, setCountry] = React.useState('');
+	const [code, setCode] = React.useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const isCorrect = code === CORRECT_CODE;
+		if (isCorrect) {
+			window.alert("You have entered the correct code");
+		} else {
+			window.alert("You have entered the wrong code");
+		}
+	}
 	const countryNames = Object.entries(COUNTRIES);
 	console.log(countryNames);
 	return (
 		<>
 			<h3>Select Country: </h3>
-			<form>
+			<form onClick={handleSubmit}>
 				<fieldset>
 					<legend>Shipping info:</legend>
 					<label htmlFor="country">Country:</label>
@@ -29,6 +41,21 @@ function Country() {
 				<p className="country-display">
 					Selected country: {COUNTRIES[country]}
 				</p>
+
+				<label htmlFor="auth-code">
+					Enter authorization code:
+				</label>
+				<div className="row">
+					<input
+						id="auth-code"
+						type="text"
+						required={true}
+						maxLength={6}
+						value={code}
+						onChange={(event) => setCode(event.target.value)}
+					/>
+					<button>Check Code</button>
+				</div>
 
 				<button>Submit</button>
 			</form>
